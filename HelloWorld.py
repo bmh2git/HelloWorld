9201d5a8c0d4b9e3e478154af70f6a431823866e
Benjamin Harristhal <benjamin.harristhal@centurylink.com>
2015-04-17 10:58:19 -0500

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+__author__ = 'hashbang'
+
+
+def run():
+    print "Hello World"
+
+
+if __name__ == "__main__":
+    run()
---
+++ TestHelloWorld.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+__author__ = 'hashbang'
+
+
+import unittest
+from
+
+class TestHelloWorld(unittest.TestCase):
+    def test_Hello(self):
+
+        self.assertTrue( 1 )
+
+
+if __name__ == "__main__":
+    unittest.main()
